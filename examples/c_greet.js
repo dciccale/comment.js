afeb3000547d2ae2c72d6af835c79aa5aae71e30
Denis Ciccale <dciccale@gmail.com>
2013-12-12 04:09:19 +0100

    Update examples and fix small issues

---
+++ examples/c_greet.js
@@ -0,0 +1,15 @@
+/*\
+ * greet
+ [ method ]
+ * Say hello to the specified name
+ > Arguments
+ - name (string) A name to say hello
+ > Usage
+ | greet('Denis');
+ > Try it
+ # <input type="text" id="name" placeholder="Enter a name...">
+ # <button id="btn">Say Hello</button>
+\*/
+function greet(name) {
+  alert('Hello ' + name);
+};
