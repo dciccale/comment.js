8e54f266e570c13fbed55d3f142a0a565afbbfe1
Denis Ciccale <dciccale@gmail.com>
2014-03-07 22:43:44 +0100

    fix parser

--- examples/example_sub_dirs/sub_dir/invertboolean.js
+++ examples/example_sub_dirs/sub_dir/invertboolean.js
@@ -6,7 +6,8 @@
  - state (boolean) some boolean...
  = (boolean) the opposite boolean passed in the arguments
  > Usage
- | something(true);
+ | something(true); // reeturns false
+ | something(false); // reeturns true
 \*/
 function invertBoolean(bool) {
   return !bool;
