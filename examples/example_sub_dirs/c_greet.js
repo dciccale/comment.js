640282e2b0209bd005188a5d1a429000477369be
dciccale <dciccale@gmail.com>
2014-08-27 22:55:20 +0200

    update examples

--- examples/example_sub_dirs/c_greet.js
+++ examples/example_sub_dirs/c_greet.js
@@ -6,9 +6,6 @@
  - name (string) A name to say hello
  > Usage
  | greet('Denis');
- > Try it
- # <input type="text" id="c_name" placeholder="Enter a name..." autocomplete="off">
- # <button id="btn">Say Hello</button>
 \*/
 function greet(name) {
   alert('Hello ' + name);
